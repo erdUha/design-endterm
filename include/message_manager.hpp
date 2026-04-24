d0cf44e87d63f824417e701508dfc41c73d5d795
erdUha <theerden.kenzhebaev.2006@gmail.com>
2026-04-24 13:04:31 +0500

    partCCC

---
+++ include/message_manager.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <yaml-cpp/yaml.h>
+
+class message_manager {
+private:
+  YAML::Node config;
+
+public:
+  message_manager(const std::string& filename) {
+    try {
+      config = YAML::LoadFile(filename);
+    } catch (const YAML::Exception& e) {
+      std::cerr << "Could not load messages: " << e.what() << std::endl;
+    }
+  }
+
+  std::string get(const std::string& category, const std::string& key) {
+    if (config[category] && config[category][key]) {
+      return config[category][key].as<std::string>();
+    }
+    return "Missing string: " + category + "." + key;
+  }
+};
--- src/partC/main.cpp
+++ src/partC/main.cpp
@@ -5,43 +5,60 @@
 #include <my_types/queue.hpp>
 #include <my_types/stack.hpp>
 #include <my_types/union_find.hpp>
-#include <yaml-cpp/yaml.h>
+#include <message_manager.hpp>
 
-class MessageManager {
-private:
-  YAML::Node config;
+message_manager msg("config/messages.yaml"); // getting messages from config
 
-public:
-  MessageManager(const std::string& filename) {
-    try {
-      config = YAML::LoadFile(filename);
-    } catch (const YAML::Exception& e) {
-      std::cerr << "Could not load messages: " << e.what() << std::endl;
-    }
-  }
-
-  std::string get(const std::string& category, const std::string& key) {
-    if (config[category] && config[category][key]) {
-      return config[category][key].as<std::string>();
-    }
-    return "Missing string: " + category + "." + key;
-  }
-} msg("config/messages.yaml");
-
-void promptMain();
+void prompt_main();
 
 int main(int argc, char** argv)
 {
-  promptMain();
+  prompt_main();
   return 0;
 }
 
-void promptMain() {
-  std::cout << msg.get("menu_prompts", "main_menu") << "\n";
-  std::cout << msg.get("menu_prompts", "main_menu_options") << "\n";
-  std::cout << "Choose: ";
-  std::string input;
-  std::cin >> input;
-  std::cout << "You chose: " << input << "\n";
-  //promptMain();
+void prompt_tasks();
+void prompt_components();
+
+void prompt_main() {
+  bool running = true;
+
+  while (running) {
+    std::cout << msg.get("menu_prompts", "main_menu") << "\n";
+    std::cout << msg.get("menu_prompts", "main_menu_options") << "\n";
+    std::cout << "Choose (0 to Exit): ";
+
+    int input;
+    if (!(std::cin >> input)) {
+      std::cout << "Invalid input! Please enter a number.\n";
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+    }
+    std::cout << "\n";
+
+    switch (input) {
+      case 0:
+        std::cout << "Exiting...\n";
+        running = false;
+        break;
+      case 1:
+        prompt_tasks();
+        break;
+      case 2:
+        prompt_components();
+        break;
+      default:
+        std::cout << "The Option is Not in the List\n";
+    }
+
+    std::cout << "\n";
+  }
+}
+
+void prompt_tasks() {
+  std::cout << "--- Tasks Module ---\n";
+}
+
+void prompt_components() {
+  std::cout << "--- Components Module ---\n";
 }
