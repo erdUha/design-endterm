0336787db75f260b9f0356c4984106ef5f0fd2ca
erdUha <theerden.kenzhebaev.2006@gmail.com>
2026-04-24 16:53:04 +0500

    update UwU

--- src/partC/main.cpp
+++ src/partC/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <set>
 #include <vector>
+#include <chrono>
+#include <iomanip>
 #include <my_types/queue.hpp>
 #include <my_types/stack.hpp>
 #include <my_types/union_find.hpp>
@@ -9,11 +12,42 @@
 
 message_manager msg("config/messages.yaml"); // getting messages from config
 
+class Task {
+public:
+  unsigned int taskID;
+  std::string description;
+  std::chrono::system_clock::time_point timestamp;
+
+  // constructor
+  Task(unsigned int newID, std::string newDescription) :
+    taskID(newID),
+    description(newDescription)
+  {
+    timestamp = std::chrono::system_clock::now();
+  }
+
+  std::string get_formatted_time() const {
+    std::time_t time_now = std::chrono::system_clock::to_time_t(timestamp);
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&time_now), "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Task& t) {
+    os << "Task #" << t.taskID << "[" << t.get_formatted_time() << "]" << ": " << t.description;
+    return os;
+  }
+};
+
+my_types::queue<Task> tasks;
+my_types::stack<Task> task_completion_history;
+
 void prompt_main();
 
 int main(int argc, char** argv)
 {
-  prompt_main();
+  prompt_main(); // main menu loop
+  tasks.enqueue(Task(1, "Fuck"));
   return 0;
 }
 
@@ -24,15 +58,16 @@ void prompt_main() {
   bool running = true;
 
   while (running) {
-    std::cout << msg.get("menu_prompts", "main_menu") << "\n";
-    std::cout << msg.get("menu_prompts", "main_menu_options") << "\n";
+    std::cout << msg.get("main_menu", "main_menu_message") << "\n";
+    std::cout << msg.get("main_menu", "main_menu_options") << "\n";
     std::cout << "Choose (0 to Exit): ";
 
     int input;
     if (!(std::cin >> input)) {
-      std::cout << "Invalid input! Please enter a number.\n";
+      std::cout << "\nInvalid input! Please enter a number.\n\n";
       std::cin.clear();
       std::cin.ignore(1000, '\n');
+      continue;
     }
     std::cout << "\n";
 
@@ -48,7 +83,7 @@ void prompt_main() {
         prompt_components();
         break;
       default:
-        std::cout << "The Option is Not in the List\n";
+        std::cout << msg.get("other", "not_in_list") << "\n";
     }
 
     std::cout << "\n";
@@ -56,9 +91,35 @@ void prompt_main() {
 }
 
 void prompt_tasks() {
-  std::cout << "--- Tasks Module ---\n";
+  bool running = true;
+
+  while (running) {
+    std::cout << msg.get("tasks_menu", "tasks_menu_message") << "\n";
+    std::cout << msg.get("tasks_menu", "tasks_menu_options") << "\n";
+    std::cout << "Choose (0 to Exit): ";
+
+    int input;
+    if (!(std::cin >> input)) {
+      std::cout << "\nInvalid input! Please enter a number.\n\n";
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      continue;
+    }
+    std::cout << "\n";
+
+    switch (input) {
+      case 0:
+        std::cout << "Exiting...\n";
+        running = false;
+        break;
+      case 1:
+        0 == 0;
+        break;
+    }
+  }
 }
 
 void prompt_components() {
-  std::cout << "--- Components Module ---\n";
+  std::cout << msg.get("components_menu", "components_menu_message") << "\n";
+  std::cout << msg.get("components_menu", "components_menu_options") << "\n";
 }
